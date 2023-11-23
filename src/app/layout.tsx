93e08cadef933406ea2516c73c017dbdb6b43877
João Bebber <joao.gbebber@gmail.com>
2023-11-23 18:45:44 -0300

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+type RootLayoutProps = {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
